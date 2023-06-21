311e726c01f9bdf424d3f9df0802324495a78304
ygk313 <ygk313@naver.com>
2023-06-21 22:22:46 +0900

    chore: Add Folders and title in 0621.cpp

--- Programmers/Level2/0621.cpp
+++ Programmers/Level2/0621.cpp
@@ -1,4 +1,4 @@
-//https://school.programmers.co.kr/learn/courses/30/lessons/154540
+//무인도 여행 : https://school.programmers.co.kr/learn/courses/30/lessons/154540
 #include <bits/stdc++.h>
 
 using namespace std;
